02d4950af78eb383ae3cfdd750037e6ca34a48ca
warramma <wishmorethewitch@gmail.com>
2024-07-03 18:06:30 -0500

    Generate random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Helloooo peoplesss!!')
+
+console.log('Helloooo peoplesss!!');
+
+function getComputerChoice(){
+    randomChoice = Math.floor((Math.random()*3));
+    console.log(randomChoice);
+    if(randomChoice === 0){
+        return 'rock';
+    }else if(randomChoice === 1){
+        return 'paper';
+    }else if(randomChoice === 2){
+        return 'scissors';
+    }else{
+        return parseInt(randomChoice) + ' is an invalid choice';
+    }
+}
+function getHumanChoice(){
+    userChoice = prompt('Choose rock, paper or scissors!');
+}
+getComputerChoice()
